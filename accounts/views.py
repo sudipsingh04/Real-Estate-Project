b6543a322b17e4ad79850be2fccda4f891a7b0c3
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-22 18:45:47 +0530

    Added login register

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # register
+        return
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # login
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
