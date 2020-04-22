d9f44b01d029b049f3215b77f6b3c5fa08793795
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-22 19:10:41 +0530

    message alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        # register
-        return
+        messages.error(request, 'Error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
